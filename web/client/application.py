079dc264c2cb89c3b0c42b33862adea588853951
Ellynna <ukaris1004@gmail.com>
2022-01-04 16:41:18 +0900

    hy 마지막

--- web/client/application.py
+++ web/client/application.py
@@ -34,10 +34,13 @@ def ootd():
 def setting():
 
     tool_tip_list=[]
+    category_list=[]
     with open('clothes.json', encoding='UTF8') as cloth_json:
         json_data = json.load(cloth_json)  # cloth_json 불러옴
         for i in range(7):
             tool_tip_list.append(json_data["closet"][i]["tool_tip"])
+            category_list.append(json_data["closet"][i]["category_to_save"])
+    tool_tip_list.append(category_list)
     return render_template("setting.html", tool_tip_list_result=tool_tip_list)
 
 
@@ -136,9 +139,6 @@ def fashion(isUpload, isAdd):
         return render_template('add_clothes.html', results=results)
 
     else:
-        if max(pred) < 0.6 and isAdd:
-            return redirect(url_for("underProb_ootd"))
-
         circle = clothOps.get_graph_key_value("circle")
         stick = clothOps.get_graph_key_value("stick")
         results = {"label": label, "category": category,
@@ -156,9 +156,6 @@ def confirm(position, category, nickname, box_num):
 def underProb():
     return render_template("underProb.html")
 
-@application.route("/underProb_ootd")
-def underProb_ootd():
-    return render_template("underProb_ootd.html")
 
 # 옷 추가
 @application.route("/box/<int:box_num>", methods=['GET'])  # 각 closet_num에 해당하는 번호의 수납함으로 이동
@@ -173,22 +170,25 @@ def box(box_num):
 @application.route("/<int:box_num>/<cloth_name>", methods=['GET'])
 def cloth_detail(box_num, cloth_name):
     # clothes.json의 closet에서 옷의 이름, 카테고리, 착용횟수, 이미지 경로, feature 경로, 최근 착용일 정보 받아옴
+    current_cloth = {}
     with open('clothes.json', encoding='UTF8') as cloth_json:
         json_data = json.load(cloth_json)
         box_data_clothes = json_data["closet"][box_num - 1]["clothes_list"]
-        current_cloth = {}
         for cloth in box_data_clothes:
             if cloth["name"] == cloth_name:
                 current_cloth = cloth
                 break
         current_cloth['box_num'] = str(box_num)
         # cloth_detail.html보면 자바스크립트 동작 안해서 count를 str로 바꿔놓음
-        current_cloth['count'] = str(current_cloth['count'])
+        current_cloth['count'] = str(current_cloth["count"])
         current_category = current_cloth["category"]
+
+    with open('opsInfo.json', encoding='UTF8') as opsCloth_json:
+        ops_json_data = json.load(opsCloth_json)
         # clothes.json의 clothes_laundry에서 해당하는 카테고리의 세탁정보 받아옴
-        # current_cloth['laundry_info'] = json_data["clothes_laundry"][0][current_category]
+        current_cloth['laundry_info'] = ops_json_data["clothes_laundry"][0][current_category]
         # clothes.json의 clothes_management에서 해당하는 카테고리의 세탁정보 받아옴
-        # current_cloth['management_info'] = json_data["clothes_management"][0][current_category]
+        current_cloth['management_info'] = ops_json_data["clothes_management"][0][current_category]
     return render_template("cloth_detail.html", result=current_cloth)
 
 """"@application.route('/setting.html')
