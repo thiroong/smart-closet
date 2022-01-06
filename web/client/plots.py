9a86e898b38e59cc0ad63200c450ffbc81a47b1b
illstandtall <illstandtall@gmail.com>
2022-01-06 14:40:04 +0900

    classification.py commit

--- web/client/plots.py
+++ web/client/plots.py
@@ -5,6 +5,17 @@ import pandas as pd
 
 
 def prob_graph(labels, pred):
+    '''
+        [argument]
+            labels      : [list] 옷의 종류 리스트
+            pred        : [list] 옷의 종류별 확률 값
+
+        [action]
+                    :
+
+        [return]
+                    : [numpy array] RGB 이미지의 넘파이 배열
+    '''
     data = pd.DataFrame(pred, labels, columns=["probability"])
     fig = px.bar(data_frame=data, x=data.index, y="probability",
                  color="probability", title="옷 분류별 확률")
